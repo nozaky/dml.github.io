a034428ada859eb3e8a74fed44825c0f0ff31f89
nozaky <dan.montero.lopez@gmail.com>
2023-12-09 13:27:43 +0100

    modified profile and background images

--- assets/js/main.js
+++ assets/js/main.js
@@ -258,3 +258,17 @@
   });
 
 })()
+function calcularEdad(fecha) {
+  var hoy = new Date();
+  var cumpleanos = new Date(fecha);
+  var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+  var m = hoy.getMonth() - cumpleanos.getMonth();
+
+  if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+      edad--;
+  }
+
+  return edad;
+}
+
+document.querySelector("#edad").textContent = calcularEdad("09/10/1995");
